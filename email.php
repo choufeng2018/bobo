6aeb5f758b2c92d6cf126d622b19e34f81f9de9b
shhuangyan1 <shhuangyan1@163.com>
2017-09-11 16:47:17 +0800

    no message

--- email.php
+++ email.php
@@ -1,5 +1,6 @@
 ﻿<?php
 header('Content-type: text/html; charset=UTF8');
+date_default_timezone_set("Asia/Shanghai");
 function check_input($value)
 {
 // 去除斜杠
@@ -48,7 +49,7 @@ require_once ('inc/email.inc.php');
 $smtpserver = "ssl://smtp.qq.com";//SMTP服务器
 $smtpserverport = "465";//SMTP服务器端口
 $smtpusermail = "395442161@qq.com";//SMTP服务器的用户邮箱
-$smtpemailto = "shhuangyan1@163.com";//发送给谁
+$smtpemailto = "bd@xuebabobo.com";//发送给谁
 $smtpuser = "395442161";//SMTP服务器的用户帐号
 $smtppass = "ewvouxhcgngbcaef";//填写SMTP服务器的用户密码，不是qq密码
 $mailsubject = "bobo官网邮件";//$con_name."发来的咨询";//邮件主题
@@ -60,10 +61,10 @@ $mailbody=$str;
 $mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
 ##########################################
 $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
-$smtp->debug = true;//是否显示发送的调试信息
+$smtp->debug = false;//是否显示发送的调试信息
 $smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
 	echo "<script style='content:text/html'; charset='utf-8';>alert('邮件发送成功!')</script>";
-	echo '<script>location.href="#"</script>';
+	echo '<script>location.href="pages/about.html"</script>';
 
 	exit();
 
